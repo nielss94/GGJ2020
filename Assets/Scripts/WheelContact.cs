bd821fa19138027ad3d4333c1953c044f53be76c
Chris <christian-boer@hotmail.com>
2020-02-01 21:42:21 +0100

    Wheel contact done

---
+++ Assets/Scripts/WheelContact.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelContact : MonoBehaviour
+{
+    public float maxExtension;
+    public float wheelRadius;
+    public GameObject wheelMesh;
+    
+    private float _currentExtension;
+    private Vector3 _initialWheelPosition;
+
+    private void Awake()
+    {
+        _initialWheelPosition = wheelMesh.transform.position;
+    }
+
+    private void Update()
+    {
+        wheelMesh.transform.position = _initialWheelPosition;
+
+        if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, maxExtension * 5))
+        {
+            _currentExtension = hit.distance - wheelRadius;
+        }
+        else
+        {
+            _currentExtension = maxExtension;
+        }
+
+        wheelMesh.transform.localPosition = new Vector3(0, -_currentExtension, 0);
+    }
+}
