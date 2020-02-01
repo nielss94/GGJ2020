4b386170253d7c9ab62d50f2b105ffedddd7008a
Chris <christian-boer@hotmail.com>
2020-02-01 05:48:13 +0100

    New car added

---
+++ Assets/Scripts/TestWheel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestWheel : MonoBehaviour
+{
+    [Header ("Suspension")]
+    public float restLength;
+    public float springTravel;
+    public float springStiffness;
+    public float damperStiffness;
+
+    private float minLength;
+    private float maxLength;
+    private float lastLength;
+    private float springLength;
+    private float springVelocity;
+    private float springForce;
+    private float damperForce;
+
+    private Vector3 suspensionForce;
+
+    [Header ("Wheel")]
+    public float wheelRadius;
+
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = transform.root.GetComponent<Rigidbody>();
+
+        minLength = restLength - springTravel;
+        maxLength = restLength + springTravel;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, maxLength + wheelRadius))
+        {
+            lastLength = springLength;
+            springLength = hit.distance - wheelRadius;
+            springLength = Mathf.Clamp(springLength, minLength, maxLength);
+            springVelocity = (lastLength - springLength) / Time.fixedDeltaTime;
+            springForce = springStiffness * (restLength - springLength);
+            damperForce = damperStiffness * springVelocity;
+
+            suspensionForce = (springForce + damperForce) * transform.up;
+
+            _rigidbody.AddForceAtPosition(suspensionForce, hit.point);
+        }
+
+    }
+}
