6966eacadf3d8bd78eecc286996d5528d22a5fdd
Niels van Dam <nielsvdam@hotmail.com>
2020-02-01 06:53:30 +0100

    rigidbodies!

---
+++ Assets/Scripts/CenterToHovercar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterToHovercar : MonoBehaviour
+{
+    private Hovercar _hovercar;
+
+    private void Awake()
+    {
+        _hovercar = FindObjectOfType<Hovercar>();
+    }
+
+    private void Update()
+    {
+        transform.position = _hovercar.transform.position;
+        transform.rotation = _hovercar.transform.rotation;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,18 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 3f;
-    [SerializeField] private Rigidbody _platformRigidbody = null;
-
-    private bool canMove = true;
+    [SerializeField] private Transform floorDetection = null;
+    
+    private bool _canMove = true;
+    private bool _isOnPlatform = false;
     
     private PlayerStation _playerStation = null;
     private Rigidbody _rigidbody = null;
     private Vector3 _move = default;
-
+    private Transform _playerSpawn = null;
+    
     private void Awake()
     {
         _playerStation = GetComponent<PlayerStation>();
@@ -23,36 +26,52 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-        GameObject tr = GameObject.Find("PlayerSpawn");
-        transform.SetParent(tr.transform);
-        transform.localPosition = Vector3.zero;
-        
+        _playerSpawn = GameObject.Find("PlayerSpawn").transform;
+        transform.position = _playerSpawn.position;
+        transform.rotation = Quaternion.identity;
+        transform.SetParent(FindObjectOfType<CenterToHovercar>().transform);
+
         _playerStation.OnActiveStationSelected += DisableMovement;
         _playerStation.OnActiveStationDeselected += EnableMovement;
+    }
+    
+    private void Update()
+    {
+        // if (_canMove)
+        // {
+        //     transform.Translate(_move * (moveSpeed * Time.deltaTime), Space.Self);
+        // }
+
+        
+
+        _rigidbody.isKinematic = _isOnPlatform;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_canMove)
+        {
+            var nextPos = _rigidbody.position + transform.TransformDirection(_move * (moveSpeed * Time.fixedDeltaTime));
+            _rigidbody.MovePosition(nextPos);
+        }
+        
+        _isOnPlatform = Physics.SphereCast(floorDetection.position, .05f, Vector3.down, out var hit, 1,
+            LayerMask.GetMask("Platform"));
     }
 
     private void EnableMovement(Station station)
     {
-        canMove = true;
+        _canMove = true;
     }
 
     private void DisableMovement(Station station)
     {
-        canMove = false;
-    }
-
-
-    private void Update()
-    {
-        if (canMove)
-        {
-            transform.Translate(_move * (moveSpeed * Time.deltaTime), Space.Self);
-        }
+        _canMove = false;
     }
 
     public void OnMove(InputValue value)
     {
         var input = value.Get<Vector2>();
-        _move = new Vector3(input.x, 0, input.y);
+        _move = new Vector3(input.x, _move.y, input.y);
     }
 }
