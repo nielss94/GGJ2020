d83e98a2d774e394bbc1ac2c1766ecad330f38bd
Niels van Dam <nielsvdam@hotmail.com>
2020-02-01 21:13:26 +0100

    start

---
+++ Assets/Scripts/Sphere/SphereControls.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereControls : MonoBehaviour
+{
+    [SerializeField] private float baseSpeed = 3f;
+    
+    private Vector3 _move;
+
+    private void Awake()
+    {
+        _move.x = baseSpeed;
+    }
+
+    private void Update()
+    {
+        transform.Rotate(_move * (baseSpeed * Time.deltaTime));
+    }
+
+    public void Rotate(Vector3 move)
+    {
+        _move = move;
+    }
+}
