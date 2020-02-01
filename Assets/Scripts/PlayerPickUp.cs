5712b2328bfae9a6886c63c48e45e9e52dae51e1
Niels van Dam <nielsvdam@hotmail.com>
2020-02-01 14:25:23 +0100

    change

---
+++ Assets/Scripts/PlayerPickUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickUp : MonoBehaviour
+{
+    [SerializeField] private float pickupRange = 1f;
+    private Resource _pickUpResource = null;
+
+
+    private void Update()
+    {
+        if (!Physics.Raycast(transform.position, transform.forward, out var hit, pickupRange,
+            LayerMask.GetMask("ConveyorBeltPart"))) return;
+        
+        if (!TryGetComponent(out Resource resource)) return;
+            
+        if (_pickUpResource != resource)
+        {
+            _pickUpResource = resource;
+        }
+    }
+
+    public void OnUse()
+    {
+        print("ASD");
+    }
+}
