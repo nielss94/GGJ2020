5b7668ed141e71026bf3a6d4d26daa38ea8c610a
Chris <christian-boer@hotmail.com>
2020-02-01 18:29:32 +0100

    constant speed

--- Assets/Scripts/Hovercar.cs
+++ Assets/Scripts/Hovercar.cs
@@ -5,17 +5,16 @@ using UnityEngine.InputSystem;
 
 public class Hovercar : MonoBehaviour
 {
+    public float baseThrust;
+    public float speedLimit;
     public float hoverForce = 9.0f;
     public float hoverHeight = 2.0f;
-    public float forwardAccelleration = 100.0f;
-    public float backwardAccelleration = 25.0f;
     public float turnStrength = 10.0f;
     public LayerMask raycastFilter;
     public GameObject[] hoverPoints;
 
     private Rigidbody _rigidBody;
     private float _deadZone = 0.1f;
-    private float _currentThrust = 0.0f;
     private float _currentTurnRate = 0.0f;
 
     private Vector3 _move;
@@ -27,17 +26,6 @@ public class Hovercar : MonoBehaviour
 
     private void Update()
     {
-        // Thrust
-        _currentThrust = 0.0f;
-        if (_move.z > _deadZone)
-        {
-            _currentThrust = _move.z * forwardAccelleration;
-        }
-        else if (_move.z < -_deadZone)
-        {
-            _currentThrust = _move.z * backwardAccelleration;
-        }
-
         // Steering
         _currentTurnRate = 0.0f;
         if (Mathf.Abs(_move.x) > _deadZone)
@@ -73,9 +61,9 @@ public class Hovercar : MonoBehaviour
         }
 
         // Thrust
-        if (Mathf.Abs(_currentThrust) > 0)
+        if (_rigidBody.velocity.sqrMagnitude <= speedLimit)
         {
-            _rigidBody.AddForce(transform.forward * _currentThrust);
+            _rigidBody.AddForce(transform.forward * baseThrust);
         }
 
         // Turn
