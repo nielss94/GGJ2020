51dba20894c4db20d79fa2f3fc16dee4f54991c7
schryliam <schryliam@gmail.com>
2020-02-01 16:08:38 +0100

    audiomanager prefab

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-    public class SoundManager : MonoBehaviour
+    public class AudioManager : MonoBehaviour
 {
     public AudioSource efxSource;                    //Drag a reference to the audio source which will play the sound effects.
     public AudioSource[] musicSource;                    //Drag a reference to the audio source which will play the music.
-    public static SoundManager instance = null;        //Allows other scripts to call functions from SoundManager.                
+    public static AudioManager instance = null;        //Allows other scripts to call functions from SoundManager.                
     public float lowPitchRange = .95f;                //The lowest a sound effect will be randomly pitched.
     public float highPitchRange = 1.05f;            //The highest a sound effect will be randomly pitched.
     int songNumber = 0;
