5c5f506df115deed4413825f6e2eed465fa673c1
Niels van Dam <nielsvdam@hotmail.com>
2020-02-02 13:04:32 +0100

    station manager

---
+++ Assets/Scripts/Stations/StationManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class StationManager : MonoBehaviour
+{
+    public static StationManager Instance { get; private set; }
+    
+    [SerializeField] private List<Station> stations = new List<Station>();
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            stations = FindObjectsOfType<Station>().ToList();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void DamageRandomStation()
+    {
+        List<Station> activeStations = stations.FindAll(s => s.IsBroken == false);
+    
+        if (activeStations.Count <= 0) return;
+        
+        int random = Random.Range(0, activeStations.Count);
+        
+        activeStations[random].Damage();
+    }
+}
