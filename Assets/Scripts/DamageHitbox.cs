2a82c527329b2e45165b9aa426118675e04448c9
Chris <christian-boer@hotmail.com>
2020-02-02 11:16:54 +0100

    Damage hitbox

---
+++ Assets/Scripts/DamageHitbox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHitbox : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            // Notify systemmanager for damage
+            Destroy(other.gameObject);
+            // AudioManager.instance.PlayCrashSound(0);
+        }
+    }
+}
