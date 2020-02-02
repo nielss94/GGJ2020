9c1e302909734b827c060cbb071eecf347651dcf
Chris <christian-boer@hotmail.com>
2020-02-02 16:08:46 +0100

    Fuel icon added

---
+++ Assets/Scripts/FuelAmountUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FuelAmountUI : MonoBehaviour
+{
+    private TextMeshProUGUI _fuelAmountText;
+    private Furnace _furnace;
+
+    private void Awake()
+    {
+        _fuelAmountText = GetComponent<TextMeshProUGUI>();
+        _furnace = FindObjectOfType<Furnace>();
+        _furnace.OnFurnaceFuelChanged += OnFuelChanged;   
+    }
+
+    private void Start()
+    {
+        OnFuelChanged(3);
+    }
+
+    private void OnFuelChanged(int fuelAmount)
+    {
+        _fuelAmountText.text = "" + fuelAmount;
+    }
+}
